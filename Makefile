3010e02ca569327b598e9dba931f6dd77e249e7a
Kenichi HIROSE <seroron@outlook.com>
2012-05-13 00:58:41 +0900

    drop animation

---
+++ Makefile
@@ -0,0 +1,2 @@
+all:
+	ghc -o hblock src/*.hs
--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE CPP, TemplateHaskell #-}
+{-# LANGUAGE CPP, TemplateHaskell, StandaloneDeriving #-}
 -----------------------------------------------------------------------------
 --
 -- Module      :  Main
@@ -32,8 +32,8 @@ data ImageSet = ImageSet {
 }
 
 data ImageObj = ImageObj {
-  x :: Int,
-  y :: Int,
+  x :: Float,
+  y :: Float,
   width :: Int,
   height :: Int,
   alpha :: Word8,
@@ -42,21 +42,22 @@ data ImageObj = ImageObj {
 
 data BlockType =
   BlockNone | BlockGuard | BlockA | BlockB | BlockC
-  deriving Eq
+  deriving (Eq, Show)
 
 data BlockState =
-  BS_Stay | BS_Dropping | BS_Removing
+  BS_Stay | BS_Dropping | BS_Removing | BS_Removed
+  deriving Eq
 
 data BlockObj = BlockObj {
   posX :: Int,
   posY :: Int,
   state :: BlockState,
   blocktype :: BlockType,
-  imageObj :: Maybe ImageObj
-} deriving Eq
+  imageObj :: ImageObj
+}
 
-nullBlockObj =
-    BlockObj (-1) (-1) BS_Stay BlockNone Nothing
+--nullBlockObj =
+--    BlockObj (-1) (-1) BS_Stay BlockNone Nothing
 
 main :: IO ()
 main = do
@@ -69,15 +70,15 @@ main = do
     SDL.quit
 
 loadImages = do
-    blockA <- SDL.loadBMP "a.bmp"
-    blockB <- SDL.loadBMP "b.bmp"
-    blockC <- SDL.loadBMP "c.bmp"
-    blockG <- SDL.loadBMP "g.bmp"
+    blockA <- SDL.loadBMP "img/a.bmp"
+    blockB <- SDL.loadBMP "img/b.bmp"
+    blockC <- SDL.loadBMP "img/c.bmp"
+    blockG <- SDL.loadBMP "img/g.bmp"
     return (ImageSet blockA blockB blockC blockG)
 
 mainLoop:: [[BlockObj]] -> ImageSet -> IO()
 mainLoop fieldList imageSet = do
-    SDL.delay 10
+    SDL.delay 20
     fieldList' <- nextFrame fieldList imageSet
     renderFrame fieldList'
 
@@ -92,9 +93,22 @@ nextFrame fieldList imageSet = do
   let mouseLeft  = any (\x -> x==ButtonLeft) mouseBtn
   let mouseRight = any (\x -> x==ButtonRight) mouseBtn
   let (x, y) = mousePos2fieldPos (mouseX, mouseY)
-  let list = if mouseLeft==True then eraseBlock fieldList else fieldList
---  let list2 = if mouseRight==True then dropBlock list else list
-  appendBlock list imageSet
+  r <- mouse mouseLeft
+  appendBlock imageSet $ setDropState $ removeBlock $
+            dropBlock $ removeBlockAnim $ renumberBlock r
+
+    where
+        mouse True = do
+--            putStrLn ""
+--            putFieldStr fieldList
+            let ret = setRemoveState fieldList
+--            putStrLn ""
+--            putFieldStr fieldList
+            return ret
+
+        mouse False =
+            return fieldList
+
 
 renderFrame:: [[BlockObj]] -> IO()
 renderFrame fieldList = do
@@ -122,8 +136,7 @@ createBlockObj x y blocktype imageset =
       | blocktype == BlockB = blockB imageset
       | blocktype == BlockC = blockC imageset
 
-    imageobj:: Maybe ImageObj
-    imageobj = Just (ImageObj (x*32) (y*32) 32 32 255 image)
+    imageobj = ImageObj (fromIntegral (x*32)) (fromIntegral ((fieldBlockMaxY-y-1)*32)) 32 32 255 image
   in
     BlockObj x y BS_Stay blocktype imageobj
 
@@ -131,10 +144,9 @@ initField:: ImageSet -> IO [[BlockObj]]
 initField imageset = do
     blocktypelist <- getBlockTypeList
     return $ splitEvery fieldBlockMaxX $
-          map
-            (\((x, y), blocktype) -> createBlockObj x y blocktype imageset)
-            zip [(x, y) | x<-[0..fieldBlockMaxX], y<-[0..fieldBlockMaxY]] $
-              take (fieldBlockMaxX*fieldBlockMaxY) $ blocktypelist
+          map (\((x, y), blocktype) -> createBlockObj x y blocktype imageset)
+                (zip [(x, y) | x<-[0..(fieldBlockMaxX-1)], y<-[0..(fieldBlockMaxY-1)]]
+                  (take (fieldBlockMaxX*fieldBlockMaxY) $ blocktypelist))
 
     -- where
     --     putGuard:: [[BlockType]] -> [[BlockType]]
@@ -179,13 +191,12 @@ renderFiled:: [[BlockObj]] -> Surface -> IO()
 renderFiled fieldList mainSurf =
   mapM_ (\b -> render (imageObj b)) $ foldl (++) [] fieldList
   where
-    render:: Maybe ImageObj -> IO Bool
-    render Nothing = return False
-
-    render (Just imageobj) = do
+    render:: ImageObj -> IO Bool
+    render imageobj = do
       SDL.setAlpha (image imageobj) [SrcAlpha] (alpha imageobj)
       SDL.blitSurface (image imageobj) Nothing mainSurf
-        (Just (Rect (x imageobj) (y imageobj) (width imageobj) (height imageobj)))
+        (Just (Rect (floor $ x imageobj) (floor $ y imageobj)
+                    (width imageobj) (height imageobj)))
 
 mousePos2fieldPos:: (Int, Int) -> (Int, Int)
 mousePos2fieldPos (x, y) =
@@ -205,26 +216,73 @@ getBlockTypeList = do
         putBlock 1 =  BlockB
         putBlock 2 =  BlockC
 
-appendBlock:: [[BlockObj]] -> ImageSet -> IO [[BlockObj]]
-appendBlock fieldList imageSet =
+appendBlock:: ImageSet -> [[BlockObj]] ->  IO [[BlockObj]]
+appendBlock imageSet fieldList =
     mapM (\(x, list) -> putBlock x list) $ zip [0..] fieldList
     where
       putBlock x list = do
         blocktypelist <- getBlockTypeList
         let len = length list
         return $
-            map (\(y, b) -> createBlockObj x y b imageSet) $
-            zip [len..] (take (fieldBlockMaxY - len) $ blocktypelist)
+            list ++ (
+            map (\(y, b) -> createBlockObj x (fieldBlockMaxY + y-len) b imageSet) $
+            zip [len..] (take (fieldBlockMaxY - len) $ blocktypelist))
 
-eraseBlock:: [[BlockObj]] -> [[BlockObj]]
-eraseBlock fieldList =
-  map (filter (/= nullBlockObj)) $
-    foldl (\fl (x, y) -> setBlock fl x y nullBlockObj) fieldList getEraseList
+setDropState:: [[BlockObj]] -> [[BlockObj]]
+setDropState =
+    mapFieldObj setDropState'
+    where
+        setDropState' px py obj
+            | (state obj) == BS_Stay
+                && (floor $ y $ imageObj obj) < defaultBlockImgPosY obj =
+                obj {posY = py, state = BS_Dropping}
+            | otherwise =
+                obj
+
+
+dropBlock:: [[BlockObj]] -> [[BlockObj]]
+dropBlock =
+    mapFieldObj dropBlock'
+    where
+        dropBlock' _ _ obj
+            | (state obj) == BS_Dropping =
+                let
+                    imgY = y $ imageObj obj
+                    defaultY = fromIntegral $ defaultBlockImgPosY obj
+                in
+                    if imgY < defaultY then setBlockImgPosY (imgY+2) obj
+                    else obj {state = BS_Stay, imageObj = (imageObj obj){y = defaultY}}
+            | otherwise =
+                obj
+
+removeBlock:: [[BlockObj]] -> [[BlockObj]]
+removeBlock =
+    map (filter (\x -> (state x) /= BS_Removed))
+
+removeBlockAnim:: [[BlockObj]] -> [[BlockObj]]
+removeBlockAnim fieldList =
+    mapFieldObj removeBlockAnim' fieldList
+    where
+        removeBlockAnim' x y blockobj
+            | (state blockobj) == BS_Removing  =
+                let
+                    a = alpha $ imageObj blockobj
+                in
+                    if a > 10 then blockobj {imageObj = ((imageObj blockobj){alpha = (a-3)})}
+                    else           blockobj {imageObj = ((imageObj blockobj){alpha = 0}), state = BS_Removed}
+
+            | otherwise =
+                blockobj
+
+
+setRemoveState:: [[BlockObj]] -> [[BlockObj]]
+setRemoveState fieldList =
+  foldl (\fl (x, y) -> changeBlock fl x y (\f -> f {state = BS_Removing})) fieldList getEraseList
   where
     getEraseList:: [(Int, Int)]
     getEraseList =
       nub $ foldl (\el pos -> baseblock pos el) []
-          　　　[(x,y) | x<-[1..fieldBlockMaxX], y<-[1..fieldBlockMaxY]]
+          　　　[(x,y) | x<-[0..fieldBlockMaxX], y<-[0..fieldBlockMaxY]]
 
     baseblock:: (Int, Int) -> [(Int, Int)] -> [(Int, Int)]
     baseblock pos eraselist =
@@ -236,7 +294,7 @@ eraseBlock fieldList =
         len = sameBlockNum fieldList x y dx dy
       in
        if len >= 4 then
-         eraselist ++ (foldl (\f n -> ((x+n*dx),(y+n*dy))) [0..len-1])
+         foldl (\el n -> ((x+n*dx),(y+n*dy)):el) eraselist [0..len-1]
        else
          eraselist
 
@@ -261,18 +319,53 @@ sameBlockNum fieldList x y dx dy =
         | otherwise =
             BlockGuard
 
-setBlock:: [[BlockObj]] -> Int -> Int -> BlockObj -> [[BlockObj]]
-setBlock fieldList x y block =
-  replaceItem x fieldList (replaceItem y (fieldList!!x) block)
+changeBlock:: [[BlockObj]] -> Int -> Int -> (BlockObj -> BlockObj) -> [[BlockObj]]
+changeBlock fieldList x y func =
+    let
+        (left, (item:right)) = splitAt y (fieldList!!x)
+    in
+        replaceItem x fieldList (left ++ [(func item)] ++ right)
 
---unsetBlock:: [[BlockObj]] -> Int -> Int -> [[BlockObj]]
---unsetBlock fieldList x y
---  replaceItem x fieldList (removeItem y (fieldList!!x))
+renumberBlock:: [[BlockObj]]  -> [[BlockObj]]
+renumberBlock =
+    mapFieldObj (\x y obj -> obj {posX = x, posY = y})
 
 
+unsetBlock:: [[BlockObj]] -> Int -> Int -> [[BlockObj]]
+unsetBlock fieldList x y =
+  replaceItem x fieldList (removeItem y (fieldList!!x))
+
+putFieldStr:: [[BlockObj]] -> IO()
+putFieldStr =
+    mapM_ putY
+    where
+        putY list = do
+            mapM_ (\x -> putStr $ show (alpha $ imageObj x) ++ " ") list
+            putStrLn ""
+
+mapFieldObj:: (Int -> Int -> BlockObj -> BlockObj) -> [[BlockObj]] -> [[BlockObj]]
+mapFieldObj func fieldList =
+    map mapY (zip [0..] fieldList)
+    where
+        mapY (x, list) =
+            map (\(y, obj) -> func x y obj) (zip [0..] list)
 
 
+setBlockImgPosY:: Float -> BlockObj -> BlockObj
+setBlockImgPosY imgy obj =
+    obj {imageObj = (imageObj obj) {y = imgy}}
 
+defaultBlockImgPosY:: BlockObj -> Int
+defaultBlockImgPosY obj =
+    ((fieldBlockMaxY-(posY obj)-1)*32)
+
+--setBlockImgPos:: BlockObj -> BlockObj
+--setBlockImgPos obj =
+--    let
+--        imgX = 50 + (posX obj)*32
+--        imgY = 50 + (fieldBlockMaxY-(posY obj))*32
+--    in
+--        obj {imageObj = ((imageObj obj) {x = imgX, y = imgY})}
 
 
 
